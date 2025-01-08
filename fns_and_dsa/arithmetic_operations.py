d0f07efe87d4b4443819aeae0542daecdb95586c
alazartsegaye <alazartsegaye0@gmail.com>
2025-01-08 13:08:18 -0800

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -20,9 +20,10 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1*num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1/num2
-        return "Error: number can't be divided by 0"
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
     else:
         return "Error: enter a valid operation!"
 
